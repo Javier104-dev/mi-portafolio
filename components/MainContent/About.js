0a72da3ebb3a522a4a0d5321cf9a66e02652c31d
Javier Villca <javieranibalvillca@gmail.com>
2023-12-03 00:06:42 -0300

    seccion about terminada, agregado efecto hover a las imagenes de training y projects

--- components/MainContent/About.js
+++ components/MainContent/About.js
@@ -6,7 +6,13 @@ const About = () => {
     <section className='about-section' id='about'>
       <h1 className='about-title'>Sobre Mí</h1>
       <p className='about-paragraph'>
-        Estoy comenzando a dar mis primeros pasos como desarrollador web, realice algunos chállenle que formaron parte de mi capacitación en Oracle Next Educación y estoy conforme con lo que logre allí si bien tengo mucho por mejorar intento hacerlo día a día con los recursos que tengo a mi alcance. Mi objetivo actual es conseguir experiencia laboral real y poder crecer en este brazo de la informática.
+        Comencé a estudiar programación a finales del 2021, encontré el curso <span className='about-paragraph__span'>r/Argentina Programa</span> de casualidad y me  animé a cursarlo, pero tuve que dejarlo por varios meses porque no me sentía preparado, pude retomarlo gracias al programa <span className='about-paragraph__span'>ONE de Oracle</span> donde aprendí mucho sobre lo básico de programación especialmente en el consumo de APIs y el Front, fue una gran ayuda para seguir avanzando.
+      </p>
+      <p className='about-paragraph'>
+        Con <span className='about-paragraph__span'>Argentina Programa 4.0</span> pude capacitarme en el área que más me llama la atención: el <span className='about-paragraph__span'>Back-end</span>. Con todo lo aprendido puede seguir y terminar el curso de r/Argentina Programa, que para mi opinión es el más completo y se enfoca más en el Back-end y las buenas prácticas de programación que allí se usan.
+      </p>
+      <p className='about-paragraph'>
+        Con todo lo que aprendí, siento que estoy preparado y tengo un nivel aceptable para poder trabajar, siempre trato de aprender y buscar las buenas prácticas que se usan actualmente. Mi siguiente paso es aprender TypeScript y seguir avanzando en la parte de seguridad de los servidores.
       </p>
     </section>
     </>
